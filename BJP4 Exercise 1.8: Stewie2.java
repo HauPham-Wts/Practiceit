18b927596cf4c19fb2ac9bfca42ec671f1ed49f9
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 23:42:34 +0700

    Create BJP4 Exercise 1.8: Stewie2.java

---
+++ BJP4 Exercise 1.8: Stewie2.java
@@ -0,0 +1,18 @@
+
+public class Stewie2 {
+	public static void main(String[] args) {
+		System.out.println("//////////////////////");
+		copy2();
+	}
+	public static void copy1() {
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+	public static void copy2() {
+		copy1();
+		copy1();
+		copy1();
+		copy1();
+		copy1();
+	}
+}
