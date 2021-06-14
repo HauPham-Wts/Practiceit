f5b6ad88f66c73416999d81d64d2c116a99b0612
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 23:40:53 +0700

    Create BJP4 Exercise 1.4: Difference.java

---
+++ BJP4 Exercise 1.4: Difference.java
@@ -0,0 +1,6 @@
+public class Difference {
+	public static void main(String[] args) {
+		System.out.println("What is the difference between\na \' and a \"?  Or between a \" and a \\\"?\n");
+		System.out.print("One is what we see when we're typing our program.\nThe other is what appears on the \"console.\"");
+	}
+}
