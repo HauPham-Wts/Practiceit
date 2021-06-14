10bd9613371bf55d0edf58a36a39af16c33aad7b
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 23:42:09 +0700

    Create BJP4 Exercise 1.7: Mantra.java

---
+++ BJP4 Exercise 1.7: Mantra.java
@@ -0,0 +1,9 @@
+public class Mantra {
+	public static void main(String[] args) {
+		System.out.println("There's one thing every coder must understand:\nThe System.out.println command.\n");
+		copy();
+	}
+	public static void copy() {
+		System.out.println("There's one thing every coder must understand:\nThe System.out.println command.");
+	}
+}
